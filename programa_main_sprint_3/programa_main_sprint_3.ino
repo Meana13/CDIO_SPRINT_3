2d5a3a9ac7637c99e4977b3bcf27861dfba0ab43
Pmg <pablomeana13@gmail.com>
2022-12-19 20:31:28 +0100

    Introducido Humedad

--- programa_main_sprint_3/programa_main_sprint_3.ino
+++ programa_main_sprint_3/programa_main_sprint_3.ino
@@ -248,7 +248,32 @@ float Calcular_temperatura(int channelValue)
   temperatura = ((Vo - b) / m);
   return (temperatura);
 }
-
+int Calcular_humedad(int channelValue)
+{
+  //Leemos del canal channelValue a través del ADS1115
+  sensorValue = ads1015.readADC_SingleEnded(channelValue);
+  //mapeamos el valor leido para un porcentaje entre 0 a 100
+  humidityValue = map(sensorValue, 22484 , 10100, 0, 100);
+  //imprimimos por pantalla si esta seco o humedo
+  //  if (sensorValue >)
+  //  {
+  //    Serial.println("El medidor esta seco");
+  //  }
+  //  else
+  //  {
+  //    Serial.println("El medidor esta mojado");
+  //  }
+  //limitamos los valores entre 0 y 100 para minimizar errores
+  if (humidityValue >= 100)
+  {
+    humidityValue = 100;
+  }
+  if (humidityValue <= 0)
+  {
+    humidityValue = 0;
+  }
+  return (humidityValue);
+}
 }
 
 void loop() {
